6a2f7506694952bbc336f3223ac136b91353890e
GtaTorrez <j.g.latem@gmail.com>
2018-03-18 17:52:05 -0400

    profesores sin loader

--- src/app/controlasistencia/asistencia.service.ts
+++ src/app/controlasistencia/asistencia.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Socket } from 'ng-socket-io';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class AsistenciaService {
